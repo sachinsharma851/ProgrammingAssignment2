fc5be0fa676f8e80ac33b69798c6114ccf19686f
sachinsharma851 <sachinsharma851@gmail.com>
2014-08-25 03:09:28 +0530

    Create cacheMatrix.R
    The cacheMatrix file for assg 2.

---
+++ cacheMatrix.R
@@ -0,0 +1,36 @@
+
+## Put comments here that give an overall description of what your
+
+
+## This function creates a special "matrix" object that can cache its inverse. variable i stores the inverse value
+
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+
+}
+
+"This function computes the inverse of the special matrix returned by makeCacheMatrix above. 
+If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache"
+""
+cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
+}
